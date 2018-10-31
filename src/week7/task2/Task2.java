b3f96d9e849d628ef88ac615abeffdbe529e86f2
duongds <buiduong76021@gmail.com>
2018-10-31 14:25:28 +0700

    test

--- src/week7/task2/Task2.java
+++ src/week7/task2/Task2.java
@@ -1,91 +1,81 @@
 package week7.task2;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Task2 {
-    public static void ArrayIndexOutOfBoundsException(int a[], int outsize) throws ArrayIndexOutOfBoundsException {
-        try{
-            System.out.println(a[outsize]);
-        }catch (Exception e){
-            throw  e;
-        }
-    }
-
-    public static void ArithmeticException(int a, int b) throws ArithmeticException{
-        try{
-            System.out.println(a/b);
-        }catch (Exception e){
-            throw  e;
-        }
-    }
-
-    public static void NullPointerException() throws NullPointerException{
-        try{
-            TestNull str = new TestNull();
-            str.printLength();
-        }catch (Exception e){
-            throw  e;
-        }
-    }
-
-    public static void FileNotFoundException() throws IOException {
-        String path ="C:\\Hoctap\\abc.txt";
-        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(path), "UTF8");
-        BufferedReader reader = new BufferedReader(inputStreamReader);
-        try{
-            String line;
-            while ((line = reader.readLine())!= null){
-                System.out.println(line);
+    public void Test1() throws NullPointerException {
+        String duong = null;
+        try {
+            if(duong.equals("ds")){
+                System.out.println("bằng nhau");
             }
-        }catch (IOException e){
-            throw e;
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerException");
         }
     }
-    public static void main (String[] args){
-        // ArrayIndexOutOfBoundsException
-        try{
-            int a[] = new int[7];
-            ArrayIndexOutOfBoundsException(a,10);
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
+    public void Test2() throws ArrayIndexOutOfBoundsException{
+        String[] str = new String[]{"A" , "B", "C"};
+        try {
+            System.out.println(str[5]);
         }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException");
+        }
+    }
 
-        // ArithmeticException
-        try{
-            ArithmeticException(7,0);
+    public void Test3()throws ArithmeticException{
+        try {
+            System.out.println(10/0);
         }catch (ArithmeticException e){
-            System.out.println(e);
+            System.out.println("ArithmeticException");
+        }
+    }
+
+    public void Test4()throws ClassCastException{
+        Object a = new String("A");
+        try {
+            System.out.println((int)a);
+        }catch (ClassCastException e){
+            System.out.println("ClassCastException");
         }
 
-        // NullPointerException
-        try{
-            NullPointerException();
-        }catch (NullPointerException e){
-            System.out.println(e);
+    }
+    public void Test5()throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("C:\\Duong.TDT"));
+        while (sc.hasNextInt()) {
+            int ds = sc.nextInt();
+            System.out.println(ds);
         }
 
-        // FileNotFoundException
-        try{
-            FileNotFoundException();
-        }catch (IOException e){
-            System.out.println(e);
+    }
+    public void Test6()throws IOException{
+        Scanner sc = new Scanner(new File("C:\\Duong.TDT"));
+        while (sc.hasNextInt()){
+            int ds1 = sc.nextInt();
+            System.out.println(ds1);
         }
+    }
+    public static void main(String[] abc)  {
+        Task2 Test  = new Task2();
+        Test.Test1();
+        Test.Test2();
+        Test.Test3();
+        Test.Test4();
+
+        try {
+            Test.Test5();
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException");
+        }
+        try {
+            Test.Test6();
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+
     }
 }
-
-class TestNull{
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void printLength(){
-        System.out.println(name.length());
-    }
-}
-
