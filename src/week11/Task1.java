3a29d3ee86d873be3329eb7163ea8a1fb8b440f8
duongds <buiduong76021@gmail.com>
2018-11-21 15:00:19 +0700

    ds

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,36 @@
 package week11;
 
 public class Task1 {
+    public <T> void swap(T[]arr, int i, int j){
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public <T extends Comparable<T>> void Sort(T[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++) {
+                if (arr[j].compareTo(arr[min]) <= 0) {
+                    min = j;
+                }
+            }
+            swap(arr,i,min);
+        }
+    }
+    public <T> void Print(T[] arr){
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] abc){
+        Task1 DS=new Task1();
+        Byte[] B={1,0};
+        Integer[] I ={3,11,7,1,22,61};
+        Double[] F={3.51,9.6,16.9,1.2,24.3,3.4};
+        Character[] C={'a','f','m','c','k','d'};
+        DS.Sort(C);
+        DS.Print(C);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,27 @@
 package week11;
 
 public class Task2 {
+    public <T> void swap(T[]arr, int i, int j){
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public<T extends Comparable<T>> void MaxArr(T[]arr){
+        int n=arr.length;
+        T max = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i].compareTo(max)> 0){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+    public static void main(String[] abc){
+        Task2 Test=new Task2();
+        Byte[] B={1,0};
+        Integer[] I ={3,11,7,1,22,61};
+        Double[] F={3.51,9.6,16.9,1.2,24.3,3.4};
+        Character[] C={'a','f','m','c','k','d'};
+        Test.MaxArr(F);
+    }
 }
